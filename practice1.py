d649c0e81d29bc3ff85972469d7f4686e3044f65
Majid Mukhtar <majidmukhtar837@gmail.com>
2023-01-30 22:31:24 -0500

    First few python practice files

---
+++ practice1.py
@@ -0,0 +1,2 @@
+print("Im not signing up for that account dude! \n" + "But thanks for the free content 😅\n" * 3)
+# This is a comment
---
+++ robot-barrista.py
@@ -0,0 +1,18 @@
+# Let's build a robot barrista
+
+print("Hello, welcome to our coffee shop")
+
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", thank you for coming in today.")
+
+menu = "Black coffee, French Vanilla, Cocunut Latte"
+
+order = input("What can I get you? We offer " + menu + "\n")
+
+print("Oh, I also love " + order + "! Splendid choice!")
+
+
+
+

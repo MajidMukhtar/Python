a9a4a4bf00ca09465ef6b54d115c12507e540c65
Majid Mukhtar <majidmukhtar837@gmail.com>
2023-02-03 21:29:08 -0500

    Added more if to Robot Barrista

--- robot-barrista.py
+++ robot-barrista.py
@@ -1,4 +1,5 @@
 # Let's build a robot barrista
+# Video Time: 9:00
 
 print("Hello, welcome to our coffee shop")
 
@@ -6,8 +7,13 @@ print("Hello, welcome to our coffee shop")
 name = input("What is your name?\n")
 
 if name == "Ben":
-  print("You're not welcome here Ben. Get out!!!")
-  exit()
+  #print("You're not welcome here Ben. Get out!!!")
+  reply = input("Are you evil? \n")
+  if reply == "Yes":
+    print("Get out of here you villain!")
+    exit()
+  else:
+    print("Wow, a Ben that isn't evil. I like you!")
 else:
   print("Hello " + name + ", thank you for coming in today.")
 
